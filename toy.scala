77d0b68652ba0a57d22ad8f62ca3753bc4c1e29f
Michael Ficarra <git@michael.ficarra.me>
2011-02-06 03:03:50 -0500

    plenty of improvements: * using symbols where appropriate * printing node numbers to make sure messages are going where they are   supposed to go * created a behaviour class so that we can legally pattern match for   function objects

--- toy.scala
+++ toy.scala
@@ -22,36 +22,63 @@ val caller = self
 val prng   = new Random()
 var nodes  = Array[Actor]()
 
-// make 5 nodes
-5 times ((whichTime) => {
+// make some nodes
+10 times ((whichTime) => {
 	nodes = nodes ++ Array(
 		actor { loop { receive {
-			case block : (Any => Unit) =>
-				block(self)
-			case "stop" =>
-				println("node received stop message")
-				caller ! "done"
+			case (from : Actor, block : NodeBehaviour) =>
+				val id = nodes.indexOf(self)
+				println("#" + id + ": received message from node #" + (nodes.indexOf(from)))
+				block.call(from)
 		}}}
 	)
 })
 
 
-// define a behaviour for these nodes (this one simply keeps passing messages
+// behaviours are just function wrappers so we can pattern match on them
+class NodeBehaviour (block : (Actor => Unit)) {
+	def call(from : Actor) : Unit = {
+		block(from)
+	}
+	override def toString() : String = block.toString()
+}
+
+// define a behaviour for these nodes (these ones simply keep passing messages
 //  to random nodes until it gets back to the originator)
-def nodeBehaviour0 (actor : Actor) : Unit = {
+
+val nodeBehaviour0 = new NodeBehaviour(b0)
+
+def b0(from : Actor) : Unit = {
 	var rand = prng.nextInt(nodes.length)
-	println("sending message to " + rand.toString())
+	val id = nodes.indexOf(self)
+	println("#" + id + ": sending message type 0 to #" + rand)
 	if(rand == 0)
-		nodes(0) ! "stop"
+		caller ! 'done
+	else if(rand % 4 == 0)
+		nodes(rand) ! (self, nodeBehaviour1)
 	else
-		nodes(rand) ! nodeBehaviour0 _
+		nodes(rand) ! (self, nodeBehaviour0)
+}
+
+val nodeBehaviour1 = new NodeBehaviour(b1)
+
+def b1(from : Actor) : Unit = {
+	var rand = prng.nextInt(nodes.length)
+	val id = nodes.indexOf(self)
+	println("#" + id + ": sending message type 1 to #" + rand)
+	if(rand == 0)
+		caller ! 'done
+	else if(rand % 4 == 0)
+		nodes(rand) ! (self, nodeBehaviour0)
+	else
+		nodes(rand) ! (self, nodeBehaviour1)
 }
 
 
 // get it started
 println("starting")
-nodes(0) ! nodeBehaviour0 _
+nodes(0) ! (self, nodeBehaviour0)
 
 
 // wait for a node to tell us that the process is finished
-receive { case "done" => println("received done message") }
+receive { case 'done => println("main thread received done notification") }
