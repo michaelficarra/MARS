f0da4e8e8d650e18521b0d7257867aaed125952d
Michael Ficarra <git@michael.ficarra.me>
2011-02-11 23:51:24 -0500

    mostly done with vector clocks

--- toy.scala
+++ toy.scala
@@ -4,48 +4,49 @@ import java.util.ArrayList
 import java.util.Random
 
 
-val caller = self
-val prng   = new Random()
-var nodes  = Seq[Actor]()
+val numberOfNodes = 10
+
+
+val main  = self
+val prng  = new Random()
+var nodes = Seq[Actor]()
+
+def nodeName(node : Actor) : String = {
+	val id = nodes indexOf node
+	if(id < 0) return "main"
+	else return "node #" + id
+}
 
 
 // make some nodes
-0 to 9 foreach ((whichTime) => {
-	var clock = 0
-	val id = whichTime
-	def log(msg : String) = println( id + " @ " + clock + ": " + msg )
-	nodes = nodes ++ Array(
-		actor { loop { receive {
-			case (from : Actor, block : NodeBehaviour, timestamp : Int) =>
-				// do some lamport clock calculations here
-				clock = clock max (timestamp + 1)
-				log("received code to execute from node #" + (nodes.indexOf(from)))
-				var times = new Array[Int](nodes.length)
-				times update (id, clock)
-				nodes filter { _ != self } foreach ((node) => {
-					node ! (self, 'clock)
-				})
-				nodes filter { _ != self } foreach ((node) => {
-					receive {
-						case (from : Actor, timestamp : Int) =>
-							times update (nodes indexOf from, timestamp)
-					}
-				})
-				println(times toSeq)
-				block(from, clock)
-			case (from : Actor, 'clock) =>
-				from ! (self, clock)
-			case _ =>
-				log("error: received bad message")
-				exit(1.asInstanceOf[AnyRef])
-		}}}
-	)
+0 until numberOfNodes foreach ((id) => {
+	var clock = new Array[Int](numberOfNodes)
+	clock padTo (numberOfNodes, 0)
+	def log(msg : String) = println( "node #" + id + ": " + msg )
+	nodes = nodes :+ actor { loop { receive {
+		case (sender : Actor, timestamp : Array[Int], block : NodeBehaviour) =>
+			log("clock before: " + clock.toList.toString)
+			clock.indices foreach ((idx : Int) => {
+				clock(idx) = clock(idx) max timestamp(idx)
+			})
+			clock(id) = clock.max + 1
+			log(" clock after: " + clock.toList.toString)
+			log("received code to execute from " + nodeName(sender))
+			block(sender, clock)
+		case (sender : Actor, 'clock) =>
+			sender ! (self, clock)
+		case (sender : Actor, 'id) =>
+			sender ! (self, id)
+		case _ =>
+			log("error: received bad message")
+			exit(1.asInstanceOf[AnyRef])
+	}}}
 })
 
 
 // behaviours are just function wrappers so we can pattern match on them
-class NodeBehaviour (block : ((Actor, Int) => Unit)) {
-	def apply(from : Actor, clock : Int) : Unit = block(from, clock)
+class NodeBehaviour (block : ((Actor, Array[Int]) => Unit)) {
+	def apply(sender : Actor, clock : Array[Int]) : Unit = block(sender, clock)
 	override def toString() : String = block.toString()
 }
 
@@ -54,46 +55,47 @@ class NodeBehaviour (block : ((Actor, Int) => Unit)) {
 
 val nodeBehaviour0 = new NodeBehaviour(b0 _)
 
-def b0(from : Actor, clock : Int) : Unit = {
-	var rand = prng.nextInt(nodes.length)
-	val id = nodes.indexOf(self)
-	print("#" + id + ": performing computation 0; ")
+def b0(sender : Actor, clock : Array[Int]) : Unit = {
+	val rand = prng.nextInt(nodes.length)
+	val time = clock(nodes indexOf self)
+	print(nodeName(self) + " @" + time + ": performing computation 0; ")
 	if(rand == 0) {
 		println("telling main thread that computation is done")
-		caller ! ((self, 'done, clock))
+		main ! ((self, clock, 'done))
 	} else if(rand % 4 == 0) {
 		println("telling #" + rand + " to perform computation 1")
-		nodes(rand) ! ((self, nodeBehaviour1, clock))
+		nodes(rand) ! ((self, clock, nodeBehaviour1))
 	} else {
 		println("telling #" + rand + " to perform computation 0")
-		nodes(rand) ! ((self, nodeBehaviour0, clock))
+		nodes(rand) ! ((self, clock, nodeBehaviour0))
 	}
 }
 
 val nodeBehaviour1 = new NodeBehaviour(b1 _)
 
-def b1(from : Actor, clock : Int) : Unit = {
-	var rand = prng.nextInt(nodes.length)
-	val id = nodes.indexOf(self)
-	print("#" + id + ": performing computation 1; ")
+def b1(sender : Actor, clock : Array[Int]) : Unit = {
+	val rand = prng.nextInt(nodes.length)
+	val time = clock(nodes indexOf self)
+	print(nodeName(self) + " @" + time + ": performing computation 1; ")
 	if(rand == 0) {
 		println("telling main thread that computation is done")
-		caller ! ((self, 'done, clock))
+		main ! ((self, clock, 'done))
 	} else if(rand % 4 == 0) {
 		println("telling #" + rand + " to perform computation 0")
-		nodes(rand) ! ((self, nodeBehaviour0, clock))
+		nodes(rand) ! ((self, clock, nodeBehaviour0))
 	} else {
 		println("telling #" + rand + " to perform computation 1")
-		nodes(rand) ! ((self, nodeBehaviour1, clock))
+		nodes(rand) ! ((self, clock, nodeBehaviour1))
 	}
 }
 
 
 // get it started
 println("starting")
-val initialTime = 0
-nodes(0) ! (self, nodeBehaviour0, initialTime)
+val initialTime = new Array[Int](numberOfNodes)
+initialTime padTo (numberOfNodes, 0)
+nodes(0) ! (self, initialTime, nodeBehaviour0)
 
 
 // wait for a node to tell us that the process is finished
-receive { case (from : Actor, 'done, timestamp : Int) => println("main thread received done notification") }
+receive { case (sender : Actor, timestamp : Array[Int], 'done) => println("main thread received done notification") }
